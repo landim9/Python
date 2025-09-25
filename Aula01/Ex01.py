06c482cf60e945f993d64eaf66d6016fe4c72474
landim9 <landimr9des@gmail.com>
2025-09-25 19:47:09 -0300

    update

---
+++ Aula01/Ex01.py
@@ -0,0 +1,12 @@
+# a) Criar um programa em Python onde o usuário irá digitar seu nome completo, idade e peso e ao final deverá mostrar todos os dados de forma personalizada;
+# Entrada de dados
+nome = input("Digite seu nome completo: ")
+idade = input("Digite sua idade: ")
+peso = input("Digite seu peso: ")
+
+# Saída personalizada
+print("\n------------------------------------------")
+print("Nome:", nome)
+print("Idade:", idade, "anos")
+print("Peso:", peso, "kg")
+print("------------------------------------------")
