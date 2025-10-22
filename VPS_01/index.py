9f0dd89137e94f86fa0a378d4e3efed99ff33844
landim9 <landimr9des@gmail.com>
2025-10-21 21:53:58 -0300

    update

--- VPS_01/index.py
+++ VPS_01/index.py
@@ -2,8 +2,8 @@ import locale
 def calculadora():
     try:
         oper = input("Digite o operador (+, -, *, /, %): ")
-        num1 = float(input("Digite o primeiro número: "))
-        num2 = float(input("Digite o segundo número: "))
+        num1 = float(input("\nDigite o primeiro número: "))
+        num2 = float(input("\nDigite o segundo número: "))
         if oper == '+':
             result = num1 + num2
         elif oper == '-':
@@ -30,11 +30,11 @@ def cota():
         2: ("Guarulhos-SP", 112),
         3: ("Campo de Marte-SP", 98),
         4: ("Pinto Martins-CE", 2922),
-        5: ("Eduardo Gomes-AM", 6294)
+        5: ("Eduardo Gomes-AM\n", 6294)
     }
 
     
-    print("\nAeroportos de destino disponíveis:")
+    print("Aeroportos de destino disponíveis:")
     for num, (nome, distancia) in aeroportos.items():
         print(f"{num}. {nome} ({distancia} KM)")
     
@@ -52,42 +52,43 @@ def cota():
             print("Valor inválido, tente novamente. Digite apenas números.")
     
     
-    print("1. Caixa Papelão")
-    print("2. Container")
+    print("\n1. Caixa Papelão")
+    print("\n2. Container")
     
     choice = input("Escolha uma opção: ")
     
     if choice == "1":
         try:
-            y = float(input("Digite a largura em Mt (Metros): ").replace(",", "."))
-            x = float(input("Digite a altura em Mt (Metros): ").replace(",", "."))
-            z = float(input("Digite a profundidade em Mt (Metros): ").replace(",", "."))
-            w = float(input("Digite o peso em Kg (Kilo): ").replace(",", "."))
+            y = float(input("\nDigite a largura em Mt (Metros): ").replace(",", "."))
+            x = float(input("\nDigite a altura em Mt (Metros): ").replace(",", "."))
+            z = float(input("\nDigite a profundidade em Mt (Metros): ").replace(",", "."))
+            w = float(input("\nDigite o peso em Kg (Kilo): ").replace(",", "."))
             f = 0.26  
+            t = int(input("\nDigite o total de itens: "))
             
-            
-            formula = ((y * x * z * w) * (1 + f) * (1 + (distancia * 9.8)))  
+            formula = ((y * x * z * w) * (1 + f) * (1 + (distancia * 9.8)) * t)  
             
             
             formatted_formula = locale.format_string('%.2f', formula, grouping=True)
-            print("Aeroporto: ", nome_aeroporto)  
-            print(f"Custo estimado de frete: R$ {formatted_formula}")
+            print("\nAeroporto: ", nome_aeroporto)  
+            print(f"\nCusto estimado de frete: R$ {formatted_formula}")
         except ValueError:
             print("Erro: Entrada inválida. Certifique-se de digitar números válidos.")
     
     elif choice == "2":
         try:
-            y = float(input("Digite a largura em Mt (Metros): ").replace(",", "."))
-            x = float(input("Digite a altura em Mt (Metros): ").replace(",", "."))
-            z = float(input("Digite a profundidade em Mt (Metros): ").replace(",", "."))
-            w = float(input("Digite o peso em Kg (Kilo): ").replace(",", "."))
+            y = float(input("\nDigite a largura em Mt (Metros): ").replace(",", "."))
+            x = float(input("\nDigite a altura em Mt (Metros): ").replace(",", "."))
+            z = float(input("\nDigite a profundidade em Mt (Metros): ").replace(",", "."))
+            w = float(input("\nDigite o peso em Kg (Kilo): ").replace(",", "."))
             f = 0.37  
+            t = int(input("\nDigite o total de itens: "))
             
-            
-            formula = ((y * x * z * w) * (1 + f) * (1 + (distancia * 9.8)))  
-            
-            print("Aeroporto: ", nome_aeroporto)  
-            print(f"Custo estimado de frete: R$ {formula:.2f}")
+            formula = ((y * x * z * w) * (1 + f) * (1 + (distancia * 9.8)) * t)  
+
+            formatted_formula = locale.format_string('%.2f', formula, grouping=True)
+            print("\nAeroporto: ", nome_aeroporto)  
+            print(f"\nCusto estimado de frete: R$ {formatted_formula}\n")
         except ValueError:
             print("Erro: Entrada inválida. Certifique-se de digitar números válidos.")
     else:
@@ -98,9 +99,10 @@ if __name__ == "__main__":
     password = "senha123"
 
     while True:
-        print("Bem-vindo ao Cotafrete! Por favor, faça login.")    
-        username = input("Usuário: ")
-        password = input("Senha: ")
+        print("\nBem-vindo ao Cotafrete! Por favor, faça login.")    
+        print("-" * 20)
+        username = input("\nUsuário: ")
+        password = input("\nSenha: ")
 
         if username == user and password == password:
             print("Login bem-sucedido!")   
@@ -110,14 +112,14 @@ if __name__ == "__main__":
             print("Usuário ou senha incorretos. Tente novamente.")
     
     while True:
-        print("COTAFRETE")
-
-        print("\nMenu:")
-        print("1. Calculadora")
-        print("2. Cotação de Frete")
-        print("3. Finalizar o Programa")
+        print("\nCOTAFRETE")
+        print("-" * 20)
+        print("\nMenu")
+        print("\n1. Calculadora")
+        print("\n2. Cotação de Frete")
+        print("\n3. Finalizar o Programa")
                     
-        choice = input("Escolha uma opção: ")
+        choice = input("\nEscolha uma opção: ")
                     
         if choice == '1':
             calculadora()   
